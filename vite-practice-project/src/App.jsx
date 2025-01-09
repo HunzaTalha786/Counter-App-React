0c9f06564787cc70f9e18ca0982715388d65bc33
Muhammad faraz khan <47053490+Muhammadfarazkhan123@users.noreply.github.com>
2025-01-09 22:27:49 +0500

    Done

--- vite-practice-project/src/App.jsx
+++ vite-practice-project/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
 
   return (
     <div>
-      <div className="main-container p-[3rem] border-[#00ffffc5] border-[3px] bg-zinc-900 min-h-fit max-w-fit m-auto rounded-[25px]  md:mt-[10rem] mt-[15rem]">
+      <div className="main-container p-[3rem] border-[#00ffffc5] border-[3px] bg-zinc-900 min-h-fit max-w-fit m-auto rounded-[25px]  md:mt-[10rem] mt-[5rem]">
         <h1 className='text-[3rem] text-[#00ffffc5] font-[italic] font-semibold'>Counter App</h1>
         <p className='mb-[1rem] text-[4.5rem] font-bold'>{counter}</p>
         <button onClick={addValue} className='bg-zinc-700  hover:text-white rounded-[50%] text-[5rem] font-bold text-[#00ffffc5] m-[7px] w-[7.5rem] text-center border-[2.5px] border-[#00ffffc5] min-h-fit'>+</button>
@@ -47,7 +47,7 @@ function App() {
         <div className="custom-alert-overlay">
           <div className="custom-alert" >
             <p>{alertMessage}</p>
-            <button onClick={closeAlert} className="close-alert-button">CLOSE</button>
+            <button onClick={closeAlert} className="close-alert-button">Close</button>
           </div>
         </div>
       )}
